a2e42f3366e9921b13fb6f614097aece69a830c1
Felipe Santana <santanafelipe.dev@gmail.com>
2023-03-14 22:30:32 -0300

    Adicionando links no rodapé

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ $(function() {
         dots: false,
         arrows: false,
         infinite: true,
-        slidesToShow: 12,
+        slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
         speed: 2000,
@@ -11,23 +11,23 @@ $(function() {
         cssEase: "linear",
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 10,
-                    slidesToScroll: 1
-                }
-            },
-            {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 8,
+                    slidesToShow: 5,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 6,
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             },
